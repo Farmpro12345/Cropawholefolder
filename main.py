83ed70eab3677e51b45b3ae4590589a377a2e881
Farmpro12345 <96308232+Farmpro12345@users.noreply.github.com>
2022-09-03 21:12:28 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from PIL import Image  # Import Image class from library.
+import glob
+from ui import pic_input
+from ui import pic_output
+from ui import cord_input
+
+image_list = []
+cropped_images = []
+
+canon = (129,129,1708,1268)
+iphone = (200,129,1708,1268)
+fail = (150,129,1514,1138)
+
+
+for filename in glob.glob(pic_input + "*.jpg"):
+    print(filename)
+    img = Image.open(filename)
+    image_list.append(img)
+
+for image in image_list:
+    image = image.crop(cord_input)
+    cropped_images.append(image)
+
+for (i, new) in enumerate(cropped_images):
+    new.save('{}{}{}'.format(pic_output,i+1,'.jpg'))
+
